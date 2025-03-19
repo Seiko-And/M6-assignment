9705afacf0e89d3cf4e16204e90064ccf1a65531
Seiko-And <seiko.a125@gmail.com>
2025-03-19 11:26:51 -0700

    changed some codes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
-window.addEventListener('load', (e)=>{
+window.addEventListener('load', () =>{
+    const form = document.querySelector('form');
+    const formElements = form.elements;
     
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        for (let i = 0; i < formElements.length; i++){
+            if (formElements[i].type !== 'submit'){
+                const display = `${formElements[i].name} : ${formElements[i].value}`;
+                console.log(display); 
+            }
+        } 
+        form.reset();
+    });      
 });
+
+    
+
+ 
+
+ 
+
+   
+ 
+  
+
+
+
